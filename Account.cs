90dc2ae5b9185465b904699fe19969909efb6b88
sofie77 <90627552+sofie77@users.noreply.github.com>
2021-09-16 16:31:31 +0100

    new updates figure out bash already

---
+++ Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supportbank
+{
+    class Account
+    {
+        // so get;set to get the information from the program and then 
+        public string Name;
+        public decimal Balance;
+
+        public Account (string name)
+        {
+            this.Name = name;
+            // a bit redundant
+            this.Balance = 0;
+
+        }
+
+    }
+}
